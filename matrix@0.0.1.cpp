4dfc6b5126acf40332a92230ab4adcce0c3467b7
Yaroslav <32066631+1knowledge1@users.noreply.github.com>
2017-10-03 18:43:39 +0300

    Update matrix@0.0.1.cpp

--- matrix@0.0.1.cpp
+++ matrix@0.0.1.cpp
@@ -4,102 +4,105 @@
 
 
 int main() {
-    int matrix_1[3][3], matrix_2[3][3], res[3][3];
-    bool failure;
-    char ch;
+	int matrix_1[3][3], matrix_2[3][3], res[3][3];
+	bool failure;
+	char ch;
 
-    for (int i = 0; i < 3; i++) {
-    for (std::string string; std::getline(std::cin, string); ) {
-        std::istringstream stream(string);
+	for (int i = 0; i < 3; i++) {
+		for (std::string string; std::getline(std::cin, string); ) {
+			std::istringstream stream(string);
 
-            for (int j = 0; j < 3; j++) {
-                if (!(stream >> matrix_1[i][j])) {
-                    failure = true;
-                    break;
-                }
-            }
-        break;
-        }
-    }
+			for (int j = 0; j < 3; j++) {
+				if (!(stream >> matrix_1[i][j])) {
+					failure = true;
+					break;
+				}
+			}
+			break;
+		}
+	}
 
+	if (failure) {																			//проверка ввода матрицы 1
+		std::cout << "An error has occured while reading numbers from line" << std::endl;
+		return 0;
+	}
 
+	std::cin >> ch;
 
-    for (int i = 0; i < 3; i++) {
-        for (std::string string; std::getline(std::cin, string); ) {
-            std::istringstream stream(string);
+	if ((ch != '+') && (ch != '-') && (ch != '*')) {										//проверка ввода символа
+		std::cout << "An error has occured while reading numbers from line" << std::endl;
+		return 0;
+	}
 
-            for (int j = 0; j < 3; j++) {
-                if (!(stream >> matrix_2[i][j])) {
-                    failure = true;
-                    break;
-                }
-            }
-            break;
-        }
-    }
+	std::cin.ignore();
 
-    std::cin>>ch;
+	for (int i = 0; i < 3; i++) {
+		for (std::string string; std::getline(std::cin, string); ) {
+			std::istringstream stream(string);
 
-    /*if (failure) {
-        std::cout << "An error has occured while reading numbers from line" << std::endl;
-        return 0;
-    }
-
-
-
-
-   if((ch != '+') && (ch != '-') && (ch != '*')) {
-       std::cout << "An error has occured while reading numbers from line" << std::endl;
-       return 0;
-    }
+			for (int j = 0; j < 3; j++) {
+				if (!(stream >> matrix_2[i][j])) {
+					failure = true;
+					break;
+				}
+			}
+			break;
+		}
+	}
 
 
-*/
+	if (!failure) {
+		switch (ch) {
 
-    if (!failure) {
-        switch (ch) {
-            case '+': {
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        res[i][j] = matrix_1[i][j] + matrix_2[i][j];
-                    }
-                }
-                break;
-            }
-
-
-            case '-': {
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        res[i][j] = matrix_1[i][j] - matrix_2[i][j];
-                    }
-                }
-                break;
-            }
-
-            case '*':{
-                int temp=0;
-                for (int i = 0; i < 3; i++) {
-                        for (int j = 0; j < 3; j++) {
-                            temp =temp + matrix_1[i][j]*matrix_2[j][i];
-                        }
-
-                }
-
-                break;}
-        }
+		case '+': {
+			std::cout << std::endl;
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					res[i][j] = matrix_1[i][j] + matrix_2[i][j];
+					std::cout << res[i][j] << ' ';
+				}
+				std::cout << std::endl;
+			}
+			
+			break;
+		}
 
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            std::cout << res[i][j] << ' ';
-        }
-        std::cout<<std::endl;
-        }
-    }
-    else {
-        std::cout << "An error has occured while reading numbers from line" << std::endl;
-    }
+		case '-': {
+			std::cout << std::endl;
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					res[i][j] = matrix_1[i][j] - matrix_2[i][j];
+					std::cout << res[i][j] << ' ';
+				}
+				std::cout << std::endl;
+			}
+			
+			break;
+		}
 
-      return 0;
+		case '*': {
+			std::cout << std::endl;
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					res[i][j] = 0;
+					for (int k = 0; k < 3; k++)
+						res[i][j] += matrix_1[i][k] * matrix_2[k][j];
+					
+					std::cout << res[i][j] << " ";
+					}
+				std::cout << std::endl;
+				}
+			
+			break; 
+			}
+		}
+
+	}
+
+	else {
+		std::cout << "An error has occured while reading numbers from line" << std::endl;
+	}
+
+	return 0;
 }
